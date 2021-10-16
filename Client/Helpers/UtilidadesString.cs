ab766ac6d87de16d314dcd45eca7d78982d9d198
Selvin Medina <selvinmedina0016@gmail.com>
2021-10-15 22:55:04 -0600

    Add project files.

---
+++ Client/Helpers/UtilidadesString.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorPeliculas.Client.Helpers
+{
+    public class UtilidadesString
+    {
+        public static string nombre = "Selvin";
+        public static string Enmayuscular(string valor) => valor.ToUpper();
+    }
+}
---
+++ Shared/Entidades/Pelicula.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class Pelicula
+
+    {
+        public string Titulo { get; set; }
+        public DateTime Lanzamiento { get; set; }
+    }
+}
